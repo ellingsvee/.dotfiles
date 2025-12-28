bc60b565f78ac3831bd2327c18d22519c7a7c26c
ellingsvee <elling.svee@gmail.com>
2025-12-28 14:59:18 +0100

    Add TY

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,36 @@
+-- return {
+--   "neovim/nvim-lspconfig",
+--   opts = {
+--     inlay_hints = { enabled = false },
+--   },
+-- }
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    -- global LSP options
     inlay_hints = { enabled = false },
+
+    servers = {
+      -- Disable Pyright variants
+      pyright = { enabled = false },
+      basedpyright = { enabled = false },
+
+      -- Enable ty
+      ty = {
+        enabled = true,
+        settings = {
+          ty = {
+            -- put Astral ty settings here if needed
+            -- example:
+            -- pythonVersion = "3.11",
+          },
+        },
+      },
+
+      -- Optional: keep Ruff if you want linting
+      ruff = {
+        enabled = true,
+      },
+    },
   },
 }
